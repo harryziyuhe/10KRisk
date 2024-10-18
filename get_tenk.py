36226a23cc14ce3b368673ae453a702e4924fda5
hfheziyu <zih028@ucsd.edu>
2024-10-18 11:18:51 -0700

    Update get_tenk.py

--- get_tenk.py
+++ get_tenk.py
@@ -8,6 +8,32 @@ from tqdm import tqdm
 
 text_elements = ['TextElement', 'TopSectionTitle', 'TitleElement']
 
+def parse_records(filings, cik):
+    accession_numbers = []
+    filing_dates = []
+    primary_documents = []
+    forms = []
+    urls = []
+    for i, accession_number in enumerate(filings['accessionNumber']):
+        filing_date = filings['filingDate'][i]
+        primary_document = filings['primaryDocument'][i]
+        form = filings['form'][i]
+        document_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number.replace('-', '')}/{primary_document}"
+
+        accession_numbers.append(accession_number)
+        filing_dates.append(filing_date)
+        primary_documents.append(primary_document)
+        forms.append(form)
+        urls.append(document_url)
+
+    return pd.DataFrame({
+        'accession': accession_numbers,
+        'time': filing_dates,
+        'primarydoc': primary_documents,
+        'form': forms,
+        'url': urls
+    })
+
 def get_records(ticker, cik = None):
     if cik is None:
         cik = get_CIK(ticker)
@@ -18,36 +44,18 @@ def get_records(ticker, cik = None):
         "User-Agent": load_identity()
     }
 
-    response = requests.get(endpoint, headers=headers)
+    data = requests.get(endpoint, headers=headers).json()
+    
+    df_records = parse_records(data['filings']['recent'], cik)
 
-    data = response.json()
-    accession_numbers = []
-    filing_dates = []
-    primary_documents = []
-    forms = []
-    urls = []
+    if len(data['filings']['files']) > 0:
+        extra_files = data['filings']['files']
+        for file in extra_files:
+            endpoint = f"https://data.sec.gov/submissions/{file['name']}"
+            data = requests.get(endpoint, headers=headers).json()
+            df_records = pd.concat([df_records, parse_records(data, cik)])
 
-    for i, accession_number in enumerate(data['filings']['recent']['accessionNumber']):
-        filing_date = data['filings']['recent']['filingDate'][i]
-        primary_document = data['filings']['recent']['primaryDocument'][i]
-        form = data['filings']['recent']['form'][i]
-        document_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number.replace('-', '')}/{primary_document}"
-
-        accession_numbers.append(accession_number)
-        filing_dates.append(filing_date)
-        primary_documents.append(primary_document)
-        forms.append(form)
-        urls.append(document_url)
-
-    df_records = pd.DataFrame({
-        'accession': accession_numbers,
-        'time': filing_dates,
-        'primarydoc': primary_documents,
-        'form': forms,
-        'url': urls
-    })
-
-    df_records.to_csv(f"data/records/{ticker}.csv")
+    df_records.to_csv(f"data/records/{ticker}.csv", index = False)
 
 def tenktext(ticker):
     if not os.path.exists(f"data/records/{ticker}.csv"):
@@ -64,10 +72,15 @@ def tenktext(ticker):
         if row['form'] == "10-K":
             accession_numbers.append(row['accession'])
             filing_dates.append(row['time'])
-            html = dl.download_filing(url=row['url']).decode()
+            try:
+                html = dl.download_filing(url=row['url']).decode()
+            except requests.exceptions.HTTPError:
+                contents.append('')
+                continue
             parser = sp.Edgar10QParser()
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore", message="Invalid section type for")
+                warnings.filterwarnings("ignore", message="Failed to get table metrics: 'NoneType' object has no attribute 'text'", category=UserWarning)
                 elements: list = parser.parse(html)
             text = ''
             for element in elements:
